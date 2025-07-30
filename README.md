401dcced42f16e983a978b9dda7688c236818f6a
David <rynstheoverlord@gmail.com>
2025-07-30 19:23:32 +0100

    new featuree!

---
+++ README.md
@@ -0,0 +1,13 @@
+# Harbinger 
+
+**Harbinger 0.1**
+
+Harbinger is a terminal companion for termux built for the purpose of acting as an all in one device manager.
+
+**Current features:**
+- Battery percent display 
+- Monitor charging state, (plugged/disconnected)
+- Monitor and display available wifi networks in the area 
+- Check internet speed [NEW]
+- Download Youtube video [NEW]
+- Monitor and alert any activity in your downloads folder [NEW]
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import time, schedule
 import os, sys, subprocess
 import random, json 
 from term_logger import Logger
-
+import threading
+import requests
+from yt_dlp import YoutubeDL
+import questionary
 
 logger = Logger()
 
@@ -47,6 +50,69 @@ battery = device.battery()
 logger.info("Welcome to Harbinger")
 logger.info(f"Battery percentage: {battery['percentage']}")
 
+
+def download_yt_video(link: str):
+    options = {
+        'format': 'bv+ba/best',
+        'merge_output_format': 'mp4',
+        'outtmpl': '~/storage/downloads/%(title)s.%(ext)s',
+        'postprocessors': [
+            {
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            },
+            {
+                'key': 'FFmpegMetadata'
+            }
+        ],
+        'postprocessor_args': [
+            '-c:v', 'libx264',  
+            '-preset', 'fast',  
+            '-c:a', 'aac',
+            '-b:a', '128k',
+            '-movflags', '+faststart'  
+        ]
+    }
+
+    with YoutubeDL(options) as ydl:
+        ydl.download([link, ])
+    os.system('am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///storage/emulated/0/Download')
+
+
+def download_yt_video_prompt():
+    link = questionary.text("Video link:").ask() 
+
+    logger.info("Starting process...")
+    download_yt_video(link=link)
+    logger.success("Video process finished.")
+
+def check_network_speed():
+    try:
+        t0 = time.time()
+        requests.get("https://www.google.com/", timeout=7)
+        t1 = time.time() 
+
+        logger.success(f"Responded in: {t1 - t0}s ")
+
+    except:
+        logger.error("You aren't connected to the internet!")
+
+
+downloads_list = []
+def watch_downloads():
+    global downloads_list
+    downloads_path = os.path.join(os.path.expanduser("~"), "storage", "downloads")
+    list_dir = os.listdir(downloads_path)
+    if downloads_list:
+        if list_dir != downloads_list:
+            additions = list(set(downloads_list) ^ set(list_dir))
+            logger.info("Noticed a new addition to downloads!")
+            for addition in additions:
+                device.notification("Harbinger", f"New download - {addition}")
+
+
+    downloads_list = list_dir
+
 current_wifi = 0
 def monitor_wifi():
     global current_wifi
@@ -73,13 +139,12 @@ def monitor_wifi():
 
 battery_count = device.battery()["percentage"]
 last_10_count = battery_count
-
+low_battery_count = 0
 charging = False 
 
 def monitor_battery():
-    global battery_count, last_10_count, charging
+    global battery_count, last_10_count, charging, low_battery_count
     output = device.battery()
-
     if output["percentage"] < battery_count:
         battery_count = output["percentage"]
         logger.info(f"New battery: {output['percentage']}%")
@@ -88,11 +153,15 @@ def monitor_battery():
         last_10_count = battery_count
         logger.warning("10 percent lost!")
 
-    if output["percentage"] < 15 and output["status"] != "charging".upper():
+    if output["percentage"] <= 15 and output["status"] != "charging".upper():
+        low_battery_count += 1
+    
+
+    if low_battery_count > (60 * 5):
         logger.error(f"Low battery, please charge! - {output['percentage']}%")
         device.vibrate()
         device.notification("Low battery", f"Only {output['percentage']} percent left!")
-        time.sleep(60 * 5)
+        low_battery_count = 0 
 
 
     if output['status'] != "CHARGING" and charging:
@@ -105,6 +174,7 @@ def monitor_battery():
     else:
         charging = False
 
+
     
 
 
@@ -112,5 +182,41 @@ def monitor_battery():
 
 schedule.every(1).seconds.do(monitor_battery)
 schedule.every(1).seconds.do(monitor_wifi)
-while True:
-    schedule.run_pending()
+schedule.every(1).seconds.do(watch_downloads)
+
+def command_func():
+    HELP = """
+h, help          => Display this message 
+q, exit, quit    => Exit program 
+cls, clear       => Clears shell
+get, fetch <url> => Send get request
+netspeed         => Checks network speed 
+yt               => Download Youtube video
+    """
+    while True:
+        command = input().lower()
+        if command in ["h", "help"]:
+            logger._log("Harbinger v 0.1", "help", show_time=False)
+            print(HELP)
+        elif command in ["q", "exit", "quit"]:
+            logger.info("Process completed.")
+            sys.exit(0)
+
+        elif command in ["clear", "cls"]:
+            subprocess.run("clear")
+
+        elif command == "netspeed":
+            check_network_speed()
+
+        elif command == "yt":
+            download_yt_video_prompt()
+
+
+
+
+def run_resender():
+    while True:
+        schedule.run_pending()
+    
+threading.Thread(target=run_resender, daemon=True).start()
+command_func()
